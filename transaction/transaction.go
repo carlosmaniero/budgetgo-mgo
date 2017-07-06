489ec5a23cb4a1356232bebbd292f841223ae914
Carlos Maniero <carlosmaniero@gmail.com>
2017-07-06 19:28:06 -0300

    Add transaction entitie with the data validation

---
+++ transaction/transaction.go
@@ -0,0 +1,45 @@
+package transaction
+
+type Transaction struct {
+	Description string
+	Amount      float64
+}
+
+type ValidationError struct {
+	message string
+}
+
+func (err *ValidationError) Error() string {
+	return err.message
+}
+
+func (entry *Transaction) AmoutValidate() error {
+	if entry.Amount == 0 {
+		return &ValidationError{"Amount can't be equal zero"}
+	}
+	return nil
+}
+
+func (entry *Transaction) DescriptionValidate() error {
+	if len(entry.Description) == 0 {
+		return &ValidationError{"Description can't be empty"}
+	}
+	return nil
+}
+
+func (entry *Transaction) Validate() []error {
+	errors := make([]error, 0)
+
+	if err := entry.AmoutValidate(); err != nil {
+		errors = append(errors, err)
+	}
+
+	if err := entry.DescriptionValidate(); err != nil {
+		errors = append(errors, err)
+	}
+
+	if len(errors) == 0 {
+		return nil
+	}
+	return errors
+}
---
+++ transaction/transaction_test.go
@@ -0,0 +1,76 @@
+package transaction
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestSpec(t *testing.T) {
+	Convey("Scenario: Validating an Transaction", t, func() {
+		Convey("Given I've an Transaction with description", func() {
+			transaction := Transaction{Description: "4 beers"}
+
+			Convey("And amount different of zero", func() {
+				transaction.Amount = 10.5
+
+				Convey("When I try to validate the transaction", func() {
+					errors := transaction.Validate()
+
+					Convey("Then the Transaction is valid", func() {
+						So(errors, ShouldBeNil)
+					})
+				})
+			})
+			Convey("And amount equal zero", func() {
+				transaction.Amount = 0
+
+				Convey("When I try to validate the transaction", func() {
+					errors := transaction.Validate()
+
+					Convey("Then the Transaction isn't valid", func() {
+						So(errors, ShouldNotBeNil)
+					})
+
+					Convey("And show me an error", func() {
+						So(errors[0].Error(), ShouldEqual, "Amount can't be equal zero")
+					})
+				})
+			})
+		})
+		Convey("Given I've an Transaction with no description", func() {
+			transaction := Transaction{Description: ""}
+
+			Convey("And amount different of zero", func() {
+				transaction.Amount = 2
+
+				Convey("When I try to validate the transaction", func() {
+					errors := transaction.Validate()
+
+					Convey("Then the Transaction isn't valid", func() {
+						So(errors, ShouldNotBeNil)
+					})
+
+					Convey("And show me an error", func() {
+						So(errors[0].Error(), ShouldEqual, "Description can't be empty")
+					})
+				})
+			})
+
+			Convey("And amount equal zero", func() {
+				transaction.Amount = 0
+
+				Convey("When I try to validate the transaction", func() {
+					errors := transaction.Validate()
+
+					Convey("Then the Transaction isn't valid", func() {
+						So(errors, ShouldNotBeNil)
+					})
+
+					Convey("And show me two errors", func() {
+						So(len(errors), ShouldEqual, 2)
+					})
+				})
+			})
+		})
+	})
+}
