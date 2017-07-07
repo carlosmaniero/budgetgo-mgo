1a93ca3b0d5448acddcd7f2b391936bafc049aa8
Carlos Maniero <carlosmaniero@gmail.com>
2017-07-07 15:23:22 -0300

    Add Funding domain
    The funding is the representation of a bank funding.
    IE: Checking account, Credit Card, Savings account.
    
    They need a name, a closing day and a limit and a amount.

---
+++ domain/funding.go
@@ -0,0 +1,50 @@
+package domain
+
+type Funding struct {
+	Name       string
+	Limit      float64
+	Amount     float64
+	ClosingDay int
+}
+
+func (funding *Funding) NameValidate() error {
+	if len(funding.Name) == 0 {
+		return &ValidationError{"Name", "can't be empty"}
+	}
+	return nil
+}
+
+func (funding *Funding) LimitValidate() error {
+	if funding.Limit < 0 {
+		return &ValidationError{"Limit", "can't be negative"}
+	}
+	return nil
+}
+
+func (funding *Funding) ClosingDayValidate() error {
+	if funding.ClosingDay <= 0 {
+		return &ValidationError{"ClosingDay", "should be greater than zero"}
+	}
+	return nil
+}
+
+func (funding *Funding) Validate() []error {
+	errors := make([]error, 0)
+
+	if err := funding.NameValidate(); err != nil {
+		errors = append(errors, err)
+	}
+
+	if err := funding.LimitValidate(); err != nil {
+		errors = append(errors, err)
+	}
+
+	if err := funding.ClosingDayValidate(); err != nil {
+		errors = append(errors, err)
+	}
+
+	if len(errors) == 0 {
+		return nil
+	}
+	return errors
+}
