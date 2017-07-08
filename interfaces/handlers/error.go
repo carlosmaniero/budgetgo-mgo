7093e5b1ffc8078742ca42b85c39aee85b8c7ad3
Carlos Maniero <carlosmaniero@gmail.com>
2017-07-08 20:00:30 -0300

    Refactor the error response handler
    Now, We've a method to get one ErrorResposeData and the
    unmarshal error handler return 400 satus code

--- interfaces/handlers/error.go
+++ interfaces/handlers/error.go
@@ -5,39 +5,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/carlosmaniero/budgetgo/interfaces/serializers"
-	"reflect"
 	"time"
 )
 
 
 func (handlers *Handlers) UnserializerErrorHandler (err error, response http.ResponseWriter) {
-	response.Header().Set("Content-Type", "application/json")
+	errorResponse := handlers.createErrorResponse(err)
+	data := serializers.SerializeErrorResponse(&errorResponse)
 
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusBadRequest)
+	fmt.Fprint(response, string(data))
+}
+
+func (handlers *Handlers) createErrorResponse(err error) serializers.ErrorResponseData{
 	switch err := err.(type) {
 	case *json.UnmarshalTypeError:
-		errorResponse := serializers.ErrorResponseData{
+		return serializers.ErrorResponseData{
 			Type: "parser",
 			Message: "cannot add " + err.Value + " value into field " +  err.Field + " of type " + err.Type.String(),
 		}
-
-		data := serializers.SerializeErrorResponse(&errorResponse)
-		fmt.Fprint(response, string(data))
 	case *time.ParseError:
-		errorResponse := serializers.ErrorResponseData{
+		return serializers.ErrorResponseData{
 			Type: "parser",
 			Message: "cannot parse the sent date. Check the date format. Date Formate: " + time.RFC3339 + " (RFC3339)",
 		}
-
-		data := serializers.SerializeErrorResponse(&errorResponse)
-		fmt.Fprint(response, string(data))
 	default:
-		fmt.Println(reflect.TypeOf(err))
-		errorResponse := serializers.ErrorResponseData{
+		return serializers.ErrorResponseData{
 			Type: "parser",
 			Message: "An error was occurred when parse your request body",
 		}
-
-		data := serializers.SerializeErrorResponse(&errorResponse)
-		fmt.Fprint(response, string(data))
 	}
 }
