ca17c142c16270e18eea0c069d25b792650e074a
Carlos Maniero <carlosmaniero@gmail.com>
2017-07-10 12:15:53 -0300

    Remove the repository factory
    The repository factory is just over engineer. It's not neccessery.
    Removed for a cleaner code.

--- interfaces/application/application.go
+++ interfaces/application/application.go
@@ -1,24 +1,18 @@
 package application
 
 import (
-	"github.com/carlosmaniero/budgetgo/interfaces/repositories"
+	"github.com/carlosmaniero/budgetgo/interfaces/repositories/memory_repository"
 	"github.com/carlosmaniero/budgetgo/usecases"
 )
 
 type Application struct {
 	TransactionRepository usecases.TransactionRepository
+	FundingRepository     usecases.FundingRepository
 }
 
 func Init() *Application {
-	registerRepositories()
-
-	repository, err := repositories.NewTransactionRepository("memory")
-
-	if err != nil {
-		panic(err)
-	}
-
 	return &Application{
-		TransactionRepository: repository,
+		TransactionRepository: memory_repository.NewMemoryTransactionRepository(),
+		FundingRepository:     memory_repository.NewMemoryFundingRepository(),
 	}
 }
