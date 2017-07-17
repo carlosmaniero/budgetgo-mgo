f4c1e80d1c7613ce2981da6756920b30b09ca2bd
Carlos Maniero <carlosmaniero@gmail.com>
2017-07-17 19:00:16 -0300

    Add lock on store a transaction
    If the lock is not provide when I register a large number of
    transactions, the transaction list is overwritten.

--- interfaces/repositories/memoryrepository/transaction_repository.go
+++ interfaces/repositories/memoryrepository/transaction_repository.go
@@ -2,6 +2,7 @@ package memoryrepository
 
 import (
 	"strconv"
+	"sync"
 
 	"github.com/carlosmaniero/budgetgo/domain"
 	"github.com/carlosmaniero/budgetgo/usecases"
@@ -12,17 +13,21 @@ import (
 // This struct will use the memory to store and retrieve all Transactions
 type MemoryTransactionRepository struct {
 	transactions []*domain.Transaction
+	mux          sync.Mutex
 }
 
 // Store a Transaction
 //
 // This mathod raises an panic if the number of 5 transactions is exceeded
 func (repository *MemoryTransactionRepository) Store(transaction *domain.Transaction) string {
+	repository.mux.Lock()
 	repository.transactions = append(repository.transactions, transaction)
-	return strconv.Itoa(len(repository.transactions))
+	id := strconv.Itoa(len(repository.transactions))
+	repository.mux.Unlock()
+	return id
 }
 
 // NewMemoryTransactionRepository Create a new transaction memory repository
 func NewMemoryTransactionRepository() usecases.TransactionRepository {
-	return &MemoryTransactionRepository{make([]*domain.Transaction, 0)}
+	return &MemoryTransactionRepository{transactions: make([]*domain.Transaction, 0)}
 }
