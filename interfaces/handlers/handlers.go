fd3d7a3520df072b80cc067cf6dbd974ace1118c
Carlos Maniero <carlosmaniero@gmail.com>
2017-07-21 13:58:49 -0300

    Remove unused files and move the repositories to main path

--- interfaces/handlers/handlers.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-import "github.com/carlosmaniero/budgetgo/interfaces/application"
-
-// Handlers contains all entrypoints handlers of the application
-type Handlers struct {
-	Application *application.Application
-}
